506b68632d92cf828ef88eb8eefee4f4970b6170
David Mikulis <davidmikulis@gmail.com>
2021-04-02 17:59:59 -0500

    Move ast to its own directory
    Split ast into a directory and split path to own module

---
+++ src/ast/mod.rs
@@ -0,0 +1,128 @@
+pub mod path;
+
+use crate::evaluate::{Context, Evaluatable, EvaluatableResult};
+use serde_json::Value;
+
+pub struct Program {
+    /// Contains the evaluation context of the program
+    ///
+    /// This tracks declared variables and functions, and
+    /// TODO the current location for reporting of runtime errors
+    ///
+    pub context: Context,
+
+    // charRange: (u32, u32)  // from 0 to number of characters (do we need this?)
+    /// Declarations of variables and functions that
+    /// occur before the return expression.
+    ///
+    /// Example:
+    /// ```
+    /// (
+    ///    $currency = Product.Price.Currency;
+    ///    $amount = Product.Price.Amount;
+    ///    $amount & $currency
+    /// )
+    /// ```
+    ///
+    /// In the example above, the variable
+    /// declarations for `$currency` and `$amount` are stored in this Vec.
+    ///
+    /// These declarations modify the internal state of the execution context while
+    /// it is running but do not return a value.
+    pub declarations: Vec<Declaration>,
+
+    /// The final expression in the JSONata program
+    /// and represents the returned value of the program.
+    ///
+    /// Example:
+    /// ```
+    /// (
+    ///    $currency = Product.Price.Currency;
+    ///    $amount = Product.Price.Amount;
+    ///    $amount & $currency
+    /// )
+    /// ```
+    ///
+    /// In this case, the `$amount & $currency` is the return expression.
+    /// This particular expression is a Concatenation Binary Expression with
+    /// a 'left' expression of the Path expression stored in `$amount` and a
+    /// 'right' expression of the Path expression in `$currency`.
+    pub return_expression: Option<Box<dyn Evaluatable>>,
+}
+
+impl Default for Program {
+    fn default() -> Self {
+        Program {
+            declarations: Default::default(),
+            return_expression: None,
+            context: Default::default(),
+        }
+    }
+}
+
+impl Program {
+    pub fn evaluate(&mut self, data: Value) -> EvaluatableResult {
+        self.set_data(data);
+        //
+        // TODO: Iterate through declarations to mutate context
+        //
+        if let Some(expr) = &self.return_expression {
+            expr.evaluate(&mut self.context)
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn set_data(&mut self, data: Value) {
+        self.context.set_data(data)
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct Location {
+    line: u32,
+    col: u32,
+    char: u64,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum Declaration {
+    Variable,
+    Function,
+}
+
+// #[derive(PartialEq, Eq, Debug, Clone)]
+// pub enum ExpressionType {
+//     Path,
+// Unary expressions
+// Unary,
+// Group,
+
+// Name,
+// String,
+// Number,
+// Value,
+// Wildcard,
+// Descendant,
+// Parent,
+// Condition,
+// Block,
+// Bind,
+// Regex,
+// Function,
+// Variable,
+// Lambda,
+// Partial,
+// Apply,
+// Transform,
+// }
+
+// #[derive(PartialEq, Eq, Debug, Clone)]
+// pub enum BinaryExpression {
+//     Numeric,
+//     Equality,
+//     Comparison,
+//     StringConcat,
+//     Range,
+//     Includes,
+// }
---
+++ src/ast/path.rs
@@ -0,0 +1,135 @@
+use serde_json::Value;
+
+use crate::evaluate::{Context, Evaluatable, EvaluatableResult};
+
+/// PathExpression is a way to get a Value from the JSON data
+///
+/// Example:
+/// ```json
+/// {
+///    "name": "ACME Corp.",
+///    "address": {
+///      "street": "Main St."
+///    }
+/// }
+/// ```
+/// The following raw expressions yield these values:
+/// * `name` --> `ACME Corp.`
+/// * `address.street` --> `Main St.`
+///
+/// The PathExpression struct holds the identifier to get from the
+/// JSON data and recursively nested PathExpression structs for any
+/// amount of nested members.
+///
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct PathExpression {
+    pub ident: String,
+    pub member: Option<Box<PathExpression>>,
+}
+
+/// Evaluates a Path expression
+impl Evaluatable for PathExpression {
+    /// Evaluate a Path expression
+    fn evaluate(&self, context: &mut Context) -> EvaluatableResult {
+        let result = self.get_member(context.data());
+        Ok(result)
+    }
+}
+
+impl PathExpression {
+    fn get_member(&self, data: &Value) -> Option<Value> {
+        if data.is_object() {
+            let value = data.get(self.ident.clone())?;
+            if let Some(m) = &self.member {
+                return m.get_member(value);
+            } else {
+                return Some(value.clone());
+            }
+        } else if let Some(arr) = data.as_array() {
+            let mut values: Vec<Value> = Vec::new();
+            for value in arr {
+                if let Some(member) = self.get_member(value) {
+                    values.push(member);
+                }
+            }
+            if values.len() > 0 {
+                return Some(Value::Array(values));
+            }
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use serde_json::json;
+
+    use crate::tests::object_data;
+
+    use super::*;
+
+    #[test]
+    fn path_get_member() {
+        let data = object_data();
+        let path = PathExpression {
+            ident: "name".to_string(),
+            member: None,
+        };
+
+        let actual = path.get_member(&data).unwrap();
+        let expected = json!("ACME Corp.");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn path_get_nested_member() {
+        let data = object_data();
+        let path = PathExpression {
+            ident: "address".to_string(),
+            member: Some(Box::new(PathExpression {
+                ident: "street".to_string(),
+                member: None,
+            })),
+        };
+
+        let actual = path.get_member(&data).unwrap();
+        let expected = json!("Main St.");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn path_get_multiple_members_from_array() {
+        let data = object_data();
+        let path = PathExpression {
+            ident: "orders".to_string(),
+            member: Some(Box::new(PathExpression {
+                ident: "id".to_string(),
+                member: None,
+            })),
+        };
+
+        let actual = path.get_member(&data).unwrap();
+        let expected = json!([1, 2]);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn path_get_nonexisting_from_array() {
+        let data = object_data();
+        let path = PathExpression {
+            ident: "orders".to_string(),
+            member: Some(Box::new(PathExpression {
+                ident: "notexist".to_string(),
+                member: None,
+            })),
+        };
+
+        let actual = path.get_member(&data);
+        assert!(actual.is_none());
+    }
+}
