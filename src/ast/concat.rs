429f621899f1e2e35a9ba542a0a1b54b87129249
David Mikulis <davidmikulis@gmail.com>
2021-04-04 21:43:06 -0500

    Implement ConcatExpression AST node
    ConcatExpressions are string concatenation binary nodes

---
+++ src/ast/concat.rs
@@ -0,0 +1,35 @@
+use serde_json::Value;
+
+use crate::evaluate::{Context, EvaluationError};
+
+use super::expression::{Expression, ExpressionType};
+
+pub struct ConcatExpression {
+    pub left: Box<dyn Expression>,
+    pub right: Box<dyn Expression>,
+}
+
+impl Expression for ConcatExpression {
+    fn etype(&self) -> ExpressionType {
+        ExpressionType::StringConcat
+    }
+
+    fn evaluate(&self, context: &Context) -> Result<Option<Value>, EvaluationError> {
+        let left_val = match self.left.evaluate(context)? {
+            Some(Value::String(s)) => s,
+            // TODO: Cast numbers, bool, null to string
+            Some(val) => "".to_string(),
+            // TODO: Cast objects/arrays to string (using serde::fmt::display?)
+            //
+            None => "".to_string(),
+        };
+        let right_val = match self.right.evaluate(context)? {
+            Some(Value::String(s)) => s,
+            // TODO: Cast numbers, bool, null to string
+            Some(val) => "".to_string(),
+            // TODO: Cast objects/arrays to string (using serde::fmt::display?)
+            None => "".to_string(),
+        };
+        Ok(Some(Value::String(left_val + &right_val)))
+    }
+}
