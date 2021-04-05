d16d0161fb9225c9011d8210aaa84bed86e538a3
David Mikulis <davidmikulis@gmail.com>
2021-04-04 21:43:54 -0500

    Rename function variable
    Use a more descriptive name for better autocomplete

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use lexer::lex_tokens;
 use parser::{parse, ParseError};
 
 mod ast;
+mod e2e;
 mod evaluate;
 mod lexer;
 mod parser;
-mod tests;
 
-pub fn jsonata(s: &str) -> Result<Program, ParseError> {
-    let tokens = lex_tokens(s);
+pub fn jsonata(input: &str) -> Result<Program, ParseError> {
+    let tokens = lex_tokens(input);
     parse(tokens.into_iter())
 }
