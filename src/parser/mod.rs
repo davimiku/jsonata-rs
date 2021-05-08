db1009a57f0f3e2e7bdf1faeaace4c4ce6711732
David Mikulis <davidmikulis@gmail.com>
2021-05-08 06:26:32 -0500

    Begin parser implementation using nom
    - Add nom dependency
    - Add parser module
    - Add string parsing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,210 +1,125 @@
-pub(crate) mod concat;
+mod ident;
+mod string;
 
-use std::iter::Peekable;
+use std::boxed::Box;
+use std::error::Error;
 
-use crate::ast::{
-    expression::Expression, literal::LiteralExpression, path::PathExpression, Program,
+use nom::{
+    bytes::complete::{is_not, tag, take_while},
+    error::{context, VerboseError},
+    number::complete::{double, float},
+    IResult,
 };
-use crate::lexer::Token;
+use nom_locate::LocatedSpan;
 
-pub fn parse<I: Iterator<Item = Token>>(tok_iter: I) -> Result<Program, ParseError> {
-    let mut parser = Parser::new(tok_iter);
-    parser.parse()
+use crate::ast::literal::LiteralValue;
+type Span<'a> = LocatedSpan<&'a str>;
+
+/// Type-erased errors
+pub type BoxError = Box<dyn Error + Send + Sync>;
+
+fn not_whitespace(i: &str) -> nom::IResult<&str, &str> {
+    is_not(" \t")(i)
 }
 
-pub struct Parser<I: Iterator<Item = Token>> {
-    tok_iter: Peekable<I>,
-
-    last_expr: Option<Box<dyn Expression>>,
+fn escaped_tab(i: &str) -> IResult<&str, &str> {
+    nom::combinator::recognize(nom::character::complete::char('\t'))(i)
 }
 
-impl<I: Iterator<Item = Token>> Parser<I> {
-    pub fn new(tok_iter: I) -> Self {
-        let peekable_iter = tok_iter.peekable();
-        Parser {
-            tok_iter: peekable_iter,
-            last_expr: None,
-        }
-    }
-
-    fn last_expr_ref(&self) -> &Option<Box<dyn Expression>> {
-        &self.last_expr
-    }
-
-    pub fn parse(&mut self) -> Result<Program, ParseError> {
-        let mut program = Program::default();
-
-        loop {
-            // could clean this up with `match` or `if let`, TBD
-            if self.tok_iter.peek().is_none() {
-                break;
-            }
-            let token = self.tok_iter.next().unwrap();
-
-            match token {
-                Token::LeftParen => {
-                    // The program may have multiple statements
-                }
-                // Token::IntegerLiteral(i) => {
-                //     let expr = LiteralExpression::from_int(i);
-                // }
-                Token::StringLiteral(s) => {
-                    self.last_expr = Some(Box::new(LiteralExpression::from_string(s)));
-                }
-                // Token::BoolLiteral(b) => {}
-                Token::Ampersand => {
-                    // String concatenation
-                    let result = self.parse_string_concat();
-                }
-                Token::Ident(s) => {
-                    let path = self.parse_path(&s)?;
-                    program.return_expression = Some(Box::new(PathExpression {
-                        ident: s,
-                        member: path.member,
-                    }));
-                }
-                Token::RightParen => {
-                    // We've safely reached the end of the program
-                }
-                _ => {}
-            }
-        }
-        Ok(program)
-    }
-
-    fn parse_path(&mut self, s: &String) -> Result<PathExpression, ParseError> {
-        let mut path = PathExpression {
-            ident: s.to_string(),
-            member: None,
-        };
-        match self.tok_iter.next() {
-            Some(Token::Dot) => {
-                // object property
-                match self.tok_iter.next() {
-                    Some(Token::Ident(s)) => {
-                        let inner_path = self.parse_path(&s)?;
-                        path.member = Some(Box::new(inner_path));
-                    }
-                    Some(_) => return Err(ParseError::NotImplemented),
-                    None => return Err(ParseError::Syntax), // unexpected end of input
-                }
-            }
-            Some(Token::LeftBracket) => {
-                // Array index, predicate expression, or array constructor
-            }
-            Some(Token::Caret) => {
-                // Order-by
-            }
-            Some(Token::LeftCurly) => {
-                // Object constructor or "reduce"
-            }
-            Some(Token::Semicolon) => {
-                // Syntax error
-            }
-
-            Some(_) => {
-                // End of path expression
-            }
-            None => {
-                // end of input, also implies end of path expression
-            }
-        }
-
-        Ok(path)
-    }
-
-    fn parse_string_literal(&mut self, s: String) -> Result<LiteralExpression, ()> {
-        Ok(LiteralExpression::from_string(s))
-    }
-
-    fn parse_integer_literal(&mut self, i: i64) -> Result<LiteralExpression, ()> {
-        Ok(LiteralExpression::from_int(i))
-    }
+fn escaped_backslash(i: &str) -> IResult<&str, &str> {
+    nom::combinator::recognize(nom::character::complete::char('\\'))(i)
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum ParseError {
-    Syntax,
+fn transform_escaped(i: &str) -> nom::IResult<&str, std::string::String> {
+    nom::bytes::complete::escaped_transform(
+        nom::bytes::complete::is_not("\\"),
+        '\\',
+        nom::branch::alt((escaped_backslash, escaped_tab)),
+    )(i)
+}
 
-    UnexpectedUnary(String), // The symbol "____" cannot be used as a unary operator
+type Res<T, U> = IResult<T, U, VerboseError<T>>;
 
-    NotImplemented, // FIXME: Implement specific errors
+fn null_literal(i: &str) -> Res<&str, LiteralValue> {
+    context("null", tag("null"))(i).map(|(next, _)| (next, LiteralValue::Null))
 }
 
 #[cfg(test)]
 mod tests {
 
-    impl<I: Iterator<Item = Token>> Parser<I> {
-        /// Advances the token iterator in the Parser
-        ///
-        /// Useful for tests to allow focusing on specific input
-        /// parsing.
-        fn advance(&mut self) {
-            self.tok_iter.next();
-        }
-    }
-
-    use std::vec::IntoIter;
-
     use super::*;
-    use crate::lexer::lex_tokens;
 
-    fn make_parser(input: &str) -> Parser<IntoIter<Token>> {
-        Parser::new(lex_tokens(input).into_iter())
-    }
-
-    #[test]
-    fn one_level_path() {
-        let input = "name";
-        let mut parser = make_parser(input);
-        parser.advance(); // simulate first token already processed
-
-        let actual = parser.parse_path(&input.to_string()).unwrap();
-        let expected = PathExpression {
-            ident: "name".to_string(),
-            member: None,
-        };
-        assert_eq!(actual, expected);
-    }
+    use nom::{
+        error::{ErrorKind, VerboseError, VerboseErrorKind},
+        Err as NomErr,
+    };
 
     #[test]
-    fn two_level_path() {
-        let input = "address.city";
-        let mut parser = make_parser(input);
-        parser.advance(); // simulate first token already processed
-
-        let actual = parser.parse_path(&"address".to_string()).unwrap();
-
-        let expected = PathExpression {
-            ident: "address".to_string(),
-            member: Some(Box::new(PathExpression {
-                ident: "city".to_string(),
-                member: None,
-            })),
-        };
-
-        assert_eq!(actual, expected);
+    fn test_null_literal() {
+        assert_eq!(null_literal("null"), Ok(("", LiteralValue::Null)));
+        assert_eq!(
+            null_literal("llun"),
+            Err(NomErr::Error(VerboseError {
+                errors: vec![
+                    ("llun", VerboseErrorKind::Nom(ErrorKind::Tag)),
+                    ("llun", VerboseErrorKind::Context("null"))
+                ],
+            }))
+        );
     }
 
-    #[test]
-    fn three_level_path() {
-        let input = "address.location.latitude";
-        let mut parser = make_parser(input);
-        parser.advance(); // simulate first token already processed
+    // #[test]
+    // fn one_level_path() {
+    //     let input = "name";
+    //     let mut parser = make_parser(input);
+    //     parser.advance(); // simulate first token already processed
 
-        let actual = parser.parse_path(&"address".to_string()).unwrap();
+    //     let actual = parser.parse_path(&input.to_string()).unwrap();
+    //     let expected = PathExpression {
+    //         ident: "name".to_string(),
+    //         member: None,
+    //     };
+    //     assert_eq!(actual, expected);
+    // }
 
-        let expected = PathExpression {
-            ident: "address".to_string(),
-            member: Some(Box::new(PathExpression {
-                ident: "location".to_string(),
-                member: Some(Box::new(PathExpression {
-                    ident: "latitude".to_string(),
-                    member: None,
-                })),
-            })),
-        };
+    // #[test]
+    // fn two_level_path() {
+    //     let input = "address.city";
+    //     let mut parser = make_parser(input);
+    //     parser.advance(); // simulate first token already processed
 
-        assert_eq!(actual, expected);
-    }
+    //     let actual = parser.parse_path(&"address".to_string()).unwrap();
+
+    //     let expected = PathExpression {
+    //         ident: "address".to_string(),
+    //         member: Some(Box::new(PathExpression {
+    //             ident: "city".to_string(),
+    //             member: None,
+    //         })),
+    //     };
+
+    //     assert_eq!(actual, expected);
+    // }
+
+    // #[test]
+    // fn three_level_path() {
+    //     let input = "address.location.latitude";
+    //     let mut parser = make_parser(input);
+    //     parser.advance(); // simulate first token already processed
+
+    //     let actual = parser.parse_path(&"address".to_string()).unwrap();
+
+    //     let expected = PathExpression {
+    //         ident: "address".to_string(),
+    //         member: Some(Box::new(PathExpression {
+    //             ident: "location".to_string(),
+    //             member: Some(Box::new(PathExpression {
+    //                 ident: "latitude".to_string(),
+    //                 member: None,
+    //             })),
+    //         })),
+    //     };
+
+    //     assert_eq!(actual, expected);
+    // }
 }
---
+++ src/parser/string.rs
@@ -0,0 +1,207 @@
+//! This module parses strings that may contain escaped characters.
+//!
+//! - String must be enclosed by double quotes
+//! - May contain any character unescaped except / and ""
+//! - Unicode code points are represented as \u{X} where X is 1-6 hex characters
+//! -
+
+use nom::{
+    branch::alt,
+    bytes::complete::{is_not, take_while_m_n},
+    combinator::{map, map_opt, map_res, verify},
+    error::{FromExternalError, ParseError},
+    multi::fold_many0,
+    sequence::{delimited, preceded},
+    IResult,
+};
+use nom::{character::complete::char as nom_char, combinator::value};
+/// Parses a str containing `u{X}` where X
+/// is a hexadecimal number {0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f}
+/// occuring between 1 and 6 times to a char (Unicode scalar value)
+fn parse_unicode<'a, E>(input: &'a str) -> IResult<&str, char, E>
+where
+    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
+{
+    // parses `u{X}` for between 1 and 6 X that are hex digits
+    let parse_hex = preceded(
+        nom_char('u'),
+        delimited(
+            nom_char('{'),
+            take_while_m_n(1usize, 6usize, |c: char| c.is_ascii_hexdigit()),
+            nom_char('}'),
+        ),
+    );
+
+    // Parses the X{1,6} string value into a u32
+    let parse_u32 = map_res(parse_hex, move |hex| u32::from_str_radix(hex, 16));
+
+    // Maps the u32 value to its char value
+    map_opt(parse_u32, |value| std::char::from_u32(value))(input)
+}
+
+/// Parses a JSONata escape character (these are the same as JSON escape characters)
+fn parse_escaped_char<'a, E>(input: &'a str) -> IResult<&'a str, char, E>
+where
+    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
+{
+    preceded(
+        nom_char('\\'),
+        alt((
+            parse_unicode,
+            value('\n', nom_char('n')),
+            value('\r', nom_char('r')),
+            value('\t', nom_char('t')),
+            value('\u{08}', nom_char('b')),
+            value('\u{0C}', nom_char('f')),
+            value('\\', nom_char('\\')),
+            value('"', nom_char('"')),
+        )),
+    )(input)
+}
+
+/// Parses text that does not contain a quote or backslash character and is not empty
+fn parse_literal<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, &'a str, E> {
+    let not_quote_or_slash = is_not("\"\\");
+
+    verify(not_quote_or_slash, |s: &str| !s.is_empty())(input)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum StringFragment<'a> {
+    Literal(&'a str),
+    EscapedChar(char),
+}
+
+/// Parses a single fragment of a string, which can be a literal str or
+/// a character that has been escaped (JSON escape char or unicode scalar value)
+fn parse_fragment<'a, E>(input: &'a str) -> IResult<&'a str, StringFragment<'a>, E>
+where
+    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
+{
+    alt((
+        map(parse_literal, StringFragment::Literal),
+        map(parse_escaped_char, StringFragment::EscapedChar),
+    ))(input)
+}
+
+/// Parses the entire string input which is defined by text surrounded
+/// by double quotes.
+/// An unescaped double quote ends the string parsing.
+///
+/// TODO: JSONata allows string literals to be delimited by single quotes
+pub fn parse_string<'a, E>(input: &'a str) -> IResult<&'a str, String, E>
+where
+    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
+{
+    let build_string = fold_many0(parse_fragment, String::new(), |mut string, fragment| {
+        match fragment {
+            StringFragment::Literal(s) => string.push_str(s),
+            StringFragment::EscapedChar(c) => string.push(c),
+        }
+        string
+    });
+
+    delimited(nom_char('"'), build_string, nom_char('"'))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::error::VerboseError;
+
+    use super::*;
+
+    #[test]
+    fn unicode_parser<'a>() {
+        let inputs = vec![
+            ("u{20}", ' '),
+            ("u{A3}", '£'),
+            ("u{100}", 'Ā'),
+            ("u{10A0}", 'Ⴀ'),
+            ("u{1F605}", '😅'),
+        ];
+
+        for (input, expected) in inputs {
+            assert_eq!(
+                Ok(("", expected)),
+                parse_unicode::<'a, VerboseError<&str>>(input)
+            );
+        }
+    }
+
+    #[test]
+    fn escaped_char_parser<'a>() {
+        let inputs = vec![
+            (r#"\n"#, '\n'),
+            (r#"\t"#, '\t'),
+            (r#"\r"#, '\r'),
+            (r#"\\"#, '\\'),
+            (r#"\""#, '"'),
+            (r#"\b"#, '\u{08}'),
+            (r#"\f"#, '\u{0C}'),
+            (r#"\u{1F605}"#, '😅'),
+        ];
+
+        for (input, expected) in inputs {
+            assert_eq!(
+                Ok(("", expected)),
+                parse_escaped_char::<'a, VerboseError<&str>>(input)
+            );
+        }
+    }
+
+    #[test]
+    fn literal_parser<'a>() {
+        let inputs = vec![
+            ("hello", "hello", ""),
+            ("before\"after", "before", "\"after"),
+            ("before\\after", "before", "\\after"),
+        ];
+
+        for (input, expected, remainder) in inputs {
+            assert_eq!(
+                Ok((remainder, expected)),
+                parse_literal::<'a, VerboseError<&str>>(input)
+            )
+        }
+    }
+
+    #[test]
+    fn fragment_parser<'a>() {
+        let inputs = vec![
+            ("hello", StringFragment::Literal("hello"), ""),
+            (r#"\\"#, StringFragment::EscapedChar('\\'), ""),
+            (
+                r#"before\\after"#,
+                StringFragment::Literal("before"),
+                r#"\\after"#,
+            ),
+            (
+                r#"\u{1F605}after"#,
+                StringFragment::EscapedChar('😅'),
+                "after",
+            ),
+        ];
+
+        for (input, expected, remainder) in inputs {
+            assert_eq!(
+                Ok((remainder, expected)),
+                parse_fragment::<'a, VerboseError<&str>>(input)
+            );
+        }
+    }
+
+    #[test]
+    fn string_parser<'a>() {
+        let inputs = vec![
+            (r#""hello""#, "hello".to_string(), ""),
+            (r#""emoji\u{1F605}""#, "emoji😅".to_string(), ""),
+        ];
+
+        for (input, expected, remainder) in inputs {
+            assert_eq!(
+                Ok((remainder, expected)),
+                parse_string::<'a, VerboseError<&str>>(input)
+            )
+        }
+    }
+}
