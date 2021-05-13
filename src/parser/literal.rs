836a7a9e88f83b920d56fa6f38e579c82b826ef3
David Mikulis <davidmikulis@gmail.com>
2021-05-13 14:50:38 -0500

    Add module that parses literal values

---
+++ src/parser/literal.rs
@@ -0,0 +1,94 @@
+//! Module parses literal JSON values including
+//!
+//! - null
+//! - true
+//! - false
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{map, value},
+    error::{FromExternalError, ParseError},
+    IResult,
+};
+
+use crate::ast::literal::{LiteralExpression, LiteralValue};
+
+use super::string::{literal_string, parse_string};
+// use crate::parser::string::parse_string;
+
+/// Parses a boolean value, either true or false
+pub(super) fn literal_bool<'a, E: ParseError<&'a str>>(
+    input: &'a str,
+) -> IResult<&'a str, LiteralValue, E> {
+    alt((
+        value(LiteralValue::Bool(true), tag("true")),
+        value(LiteralValue::Bool(false), tag("false")),
+    ))(input)
+}
+
+/// Parses the literal value `null`
+pub(super) fn literal_null<'a, E: ParseError<&'a str>>(
+    input: &'a str,
+) -> IResult<&'a str, LiteralValue, E> {
+    value(LiteralValue::Null, tag("null"))(input)
+}
+
+pub(super) fn literal_expression<'a, E>(input: &'a str) -> IResult<&'a str, LiteralExpression, E>
+where
+    E: ParseError<&'a str> + FromExternalError<&'a str, std::num::ParseIntError>,
+{
+    map(alt((literal_bool, literal_null, literal_string)), |val| {
+        val.into()
+    })(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::error::ErrorKind;
+
+    use super::*;
+
+    #[test]
+    fn bool_parser_true() {
+        let input = "true";
+        let res = literal_bool::<(&str, ErrorKind)>(input);
+        assert_eq!(res.unwrap().1, LiteralValue::Bool(true))
+    }
+
+    #[test]
+    fn bool_parser_false() {
+        let input = "false";
+        let res = literal_bool::<(&str, ErrorKind)>(input);
+        assert_eq!(res.unwrap().1, LiteralValue::Bool(false));
+    }
+
+    #[test]
+    fn null_parser() {
+        let input = "null";
+        let res = literal_null::<(&str, ErrorKind)>(input);
+        assert_eq!(res.unwrap().1, LiteralValue::Null)
+    }
+
+    #[test]
+    fn literal_expression_parser() {
+        assert_eq!(
+            literal_expression::<(&str, ErrorKind)>("true").unwrap().1,
+            LiteralExpression::from(true)
+        );
+        assert_eq!(
+            literal_expression::<(&str, ErrorKind)>("false").unwrap().1,
+            LiteralExpression::from(false)
+        );
+        assert_eq!(
+            literal_expression::<(&str, ErrorKind)>("null").unwrap().1,
+            LiteralExpression::from(LiteralValue::Null)
+        );
+        assert_eq!(
+            literal_expression::<(&str, ErrorKind)>(r#""test""#)
+                .unwrap()
+                .1,
+            LiteralExpression::from(LiteralValue::from("test"))
+        )
+    }
+}
