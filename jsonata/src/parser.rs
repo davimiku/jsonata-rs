4775cab545781215407f0fb4028279f01d4903cf
David Mikulis <davidmikulis@gmail.com>
2021-12-21 14:16:34 -0600

    ✨ Implement whitespace parsing

--- jsonata/src/parser.rs
+++ jsonata/src/parser.rs
@@ -44,13 +44,20 @@ impl<'l, 'input> Parser<'l, 'input> {
         self.events
     }
 
-    fn peek(&self) -> Option<SyntaxKind> {
+    fn peek(&mut self) -> Option<SyntaxKind> {
+        self.eat_whitespace();
+        self.peek_raw()
+    }
+
+    fn peek_raw(&self) -> Option<SyntaxKind> {
         self.lexemes
             .get(self.cursor)
             .map(|Lexeme { kind, .. }| *kind)
     }
 
     fn bump(&mut self) {
+        self.eat_whitespace();
+
         let Lexeme { kind, text } = self.lexemes[self.cursor];
 
         self.cursor += 1;
@@ -58,6 +65,12 @@ impl<'l, 'input> Parser<'l, 'input> {
             kind,
             text: text.into(),
         });
+    }
+
+    fn eat_whitespace(&mut self) {
+        while self.peek_raw() == Some(SyntaxKind::Whitespace) {
+            self.cursor += 1;
+        }
     }
 
     fn start_node(&mut self, kind: SyntaxKind) {
@@ -187,6 +200,71 @@ Root@0..14
   RParen@11..12 ")"
   RParen@12..13 ")"
   RParen@13..14 ")""#]],
+        );
+    }
+
+    #[test]
+    fn parse_whitespace() {
+        check(
+            "   ",
+            expect![[r#"
+Root@0..3
+  Whitespace@0..3 "   ""#]],
+        );
+    }
+
+    #[test]
+    fn parse_number_preceded_by_whitespace() {
+        check(
+            "   9876",
+            expect![[r#"
+Root@0..7
+  Whitespace@0..3 "   "
+  Number@3..7 "9876""#]],
+        );
+    }
+
+    #[test]
+    fn parse_number_followed_by_whitespace() {
+        check(
+            "999   ",
+            expect![[r#"
+Root@0..6
+  Number@0..3 "999"
+  Whitespace@3..6 "   ""#]],
+        );
+    }
+
+    #[test]
+    fn parse_number_surrounded_by_whitespace() {
+        check(
+            " 123     ",
+            expect![[r#"
+Root@0..9
+  Whitespace@0..1 " "
+  Number@1..4 "123"
+  Whitespace@4..9 "     ""#]],
+        );
+    }
+
+    #[test]
+    fn parse_binary_expression_with_whitespace() {
+        check(
+            " 1 +   2* 3 ",
+            expect![[r#"
+Root@0..12
+  Whitespace@0..1 " "
+  BinaryExpr@1..12
+    Number@1..2 "1"
+    Whitespace@2..3 " "
+    Plus@3..4 "+"
+    Whitespace@4..7 "   "
+    BinaryExpr@7..12
+      Number@7..8 "2"
+      Star@8..9 "*"
+      Whitespace@9..10 " "
+      Number@10..11 "3"
+      Whitespace@11..12 " ""#]],
         );
     }
 }
--- jsonata/src/parser/sink.rs
+++ jsonata/src/parser/sink.rs
@@ -1,10 +1,14 @@
 use super::event::Event;
-use crate::{lexer::Lexeme, syntax::JsonataLanguage};
+use crate::{
+    lexer::{Lexeme, SyntaxKind},
+    syntax::JsonataLanguage,
+};
 use rowan::{GreenNode, GreenNodeBuilder, Language};
 
 pub(super) struct Sink<'l, 'input> {
     builder: GreenNodeBuilder<'static>,
     lexemes: &'l [Lexeme<'input>],
+    cursor: usize,
     events: Vec<Event>,
 }
 
@@ -13,6 +17,7 @@ impl<'l, 'input> Sink<'l, 'input> {
         Self {
             builder: GreenNodeBuilder::new(),
             lexemes,
+            cursor: 0,
             events,
         }
     }
@@ -22,10 +27,10 @@ impl<'l, 'input> Sink<'l, 'input> {
         // because Rowan's Checkpoint has private members and cannot be constructed
         // manually, so Rowan's GreenNodeBuilder::start_node_at cannot be used in this way.
         let mut reordered_events = self.events.clone();
-        for (i, event) in self.events.into_iter().enumerate() {
+        for (i, event) in self.events.iter().enumerate() {
             if let Event::StartNodeAt { kind, checkpoint } = event {
                 reordered_events.remove(i);
-                reordered_events.insert(checkpoint, Event::StartNode { kind });
+                reordered_events.insert(*checkpoint, Event::StartNode { kind: *kind });
             }
         }
 
@@ -34,15 +39,31 @@ impl<'l, 'input> Sink<'l, 'input> {
                 Event::StartNode { kind } => {
                     self.builder.start_node(JsonataLanguage::kind_to_raw(kind))
                 }
-                Event::AddToken { kind, text } => self
-                    .builder
-                    .token(JsonataLanguage::kind_to_raw(kind), &text),
+                Event::AddToken { kind, text } => self.token(kind, text),
                 Event::FinishNode => self.builder.finish_node(),
                 // Unreachable due to mutation above
-                Event::StartNodeAt { kind, checkpoint } => unreachable!(),
+                Event::StartNodeAt { .. } => unreachable!(),
             }
+
+            self.eat_whitespace();
         }
 
         self.builder.finish()
+    }
+
+    fn eat_whitespace(&mut self) {
+        while let Some(lexeme) = self.lexemes.get(self.cursor) {
+            if lexeme.kind != SyntaxKind::Whitespace {
+                break;
+            }
+
+            self.token(lexeme.kind, lexeme.text.into());
+        }
+    }
+
+    fn token(&mut self, kind: SyntaxKind, text: String) {
+        self.builder
+            .token(JsonataLanguage::kind_to_raw(kind), &text);
+        self.cursor += 1;
     }
 }
